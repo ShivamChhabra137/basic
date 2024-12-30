70cb613ef5a53d5ced7507e878003ac871142c5f
Shivam <shivamchhabra137@gmail.com>
2024-12-30 21:41:54 +0530

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
-const port = process.env.PORT
+const port = 3000
 
 app.get("/", (req, res) => {
   res.send("Hello World")
